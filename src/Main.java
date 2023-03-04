e0b9cf9d0d333bf58a475fce0a035f40abd7a468
kamil <kamilolszowiec123@gmail.com>
2023-03-04 17:20:57 +0100

    lab1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Point p1=new Point(3,6);
+        Point p2=new Point(1,5);
+        Segment seg=new Segment(p1,p2);
+
+        System.out.println(seg.distance());
+
+        System.out.println(seg.toSvg());
     }
 }
--- src/Segment.java
+++ src/Segment.java
@@ -1,18 +1,19 @@
 public class Segment {
     private Point p1,p2;
-    public void setP1(Point p1) {
-        this.p1 = p1;
+    public Segment(Point p1,Point p2){
+        this.p1=p1;
+        this.p2=p2;
     }
     public Point getP1() {
         return p1;
-    }
-    public void setP2(Point p2){
-        this.p2 = p2;
     }
     public Point getP2() {
         return p2;
     }
     public double distance(){
         return Math.sqrt(Math.pow(p1.x-p2.x,2)+Math.pow(p1.y- p2.y,2));
+    }
+    public String toSvg(){
+        return String.format("<line x1=\"%.2f\" y1=\"%.2f\" x2=\"%.2f\" y2=\"%.2f\" style=\"stroke:rgb(255,0,0);stroke-width:2\" />",p1.x,p1.y,p2.x,p2.y);
     }
 }
