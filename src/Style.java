bb19a75cad883919f5b3404e4eee4898ddcbdd39
kamil <kamilolszowiec123@gmail.com>
2023-03-09 14:21:12 +0100

    lab1

--- src/Style.java
+++
@@ -1,9 +0,0 @@
-public class Style {
-    public final string fillColor,strokeColor;
-    public final double strokeWidth;
-    Style(string fillColor,string strokeColor,double strokeWidth){
-        this.fillColor=fillColor;
-        this.strokeColor=strokeColor;
-        this.strokeWidth=strokeWidth;
-    }
-}
